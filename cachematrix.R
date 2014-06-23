351d69cc122816cdbda20db7083dcf605b86ceb6
SaravShah <saravsshah@gmail.com>
2014-06-22 21:41:45 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -6,10 +6,13 @@
 makeCacheMatrix <- function(x = matrix()) {
 
 }
+## MakecacheMatrix creates a special vector which is really a list containg a function to find the inverse of a matrix
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ##this will calculate the mean of the special vector, it firsts checks to see if the mean has been calculated, then it will get the mean from the cache and skip the computation
+        
+  ## Return a matrix that is the inverse of 'x'
 }
